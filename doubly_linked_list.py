43033fbb19efc165406e824f40e2824fbd04fc80
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-03-22 21:26:58 -0400

    Create doubly_linked_list.py

---
+++ doubly_linked_list.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, curr_val, prev_val):
+        self.val = curr_val
+        self.next = None
+        if prev_val is not None:
+            self.prev = prev_val
+    
+    def insert(self, val):
+        self.next = Node(val, prev_val=self)
+
+class Doubly_Linked_List():
+    def __init__(self, val):
+        self.head = Node(val, None)
+    
